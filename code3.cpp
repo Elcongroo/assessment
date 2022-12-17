033dffac5860b9221693b9e3f2711efbde4d1f93
congroo <2823223506@qq.com>
2022-12-17 17:05:39 +0800

    code3

---
+++ code3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    int cnt;
+    char ans[100010];
+    cin>>n;
+    while(n!=1){
+        if(n%2==0){
+            n/=2;
+            ans[cnt++]='^';
+        }
+        else if(n%2==1){
+            n-=1;
+            ans[cnt++]='-';
+        }
+    }
+    cout<<cnt<<endl;
+    for(int i=0;i<cnt;i++){
+        cout<<ans[i];
+    }
+    return 0;
+}

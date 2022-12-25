6d0d24145982ad3957fa60d3729c5ce3eb173de9
congroo <2823223506@qq.com>
2022-12-25 22:33:10 +0800

    update

--- code2(unfinished).cpp
+++ code2(unfinished).cpp
@@ -1,79 +1,44 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int arr[15][15],m,n;
-int a[15][15],c[10010],num;
-bool flag[15][15];
+int arr[10][10],q[1001];
+int m,n;
 
-
-void before(){
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    return;
-}
-
-void after(){
-    int i=0,j=0,dirx=1,diry=0;
-    int total=m*n;
-    while(num<total){
-        c[num++]=a[i][j];
-        flag[i][j]=1;
-        i+=dirx,j+=diry;
-        if(i>m-1||flag[i][j]){
-            i-=dirx,j-=diry;
-            dirx=-1,diry=0;
-            i+=dirx,j+=diry;
-        }
-        else if(j>n-1||flag[i][j]){
-            i-=dirx,j-=diry;
-            dirx=0,diry=1;
-            i+=dirx,j+=diry;
-        }
-        else if(i<0||flag[i][j]){
-            i-=dirx,j-=diry;
-            dirx=0,diry=-1;
-            i+=dirx,j+=diry;
-        }
-        else if(j<0||flag[i][j]){
-            i-=dirx,j-=diry;
-            dirx=1,diry=0;
-            i+=dirx,j+=diry;
-        }
-    }
-    int t=1;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            a[i][j]=c[t];
-            t++;
-            if(t==num) t=0;
-        }
-    }
-
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cout<<a[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-int main(){
-    srand((unsigned)time(NULL));
-    m=rand()%5+3;
-    n=rand()%5+3;
+void create(){
+    int cnt=0;
+    srand((int)time(NULL));
+    m=rand()%6+2;
+    n=rand()%6+2;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             arr[i][j]=rand()%10;
+            q[cnt++]=a[i][j];
         }
     }
-    cout<<m<<n<<endl;
-    cout<<"before:"<<endl;
-    before();
-    cout<<"after:"<<endl;
-    after();
+    return ;
+}
+
+void myprint(){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<setw(5);
+        }
+        cout<<endl;
+    }
+}
+
+void move(){
+    int order=0,
+}
+
+int main(){
+    create();
+    cout<<m<<" "<<n<<endl;
+    cout<<"原矩阵为："<<endl;
+    myprint();
+    cout<<endl;
+    cout<<"现矩阵为："<<endl;
+    move();
+    myprint();
     return 0;
 }

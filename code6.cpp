9b715f6c9b46b3ae8cb2d4d1112710d6fcca3e7b
congroo <2823223506@qq.com>
2022-12-28 10:15:50 +0800

    剩下第六题

--- code6.cpp
+++ code6.cpp
@@ -4,40 +4,26 @@ using namespace std;
 int a,b,c,d,ansa,ansb,ansc,ansd;
 
 int check(){
-    if(a==ansa&&b==ansb&&c==ansc&&d==ansd)
+    if(a>=ansa&&b>=ansb&&c>=ansc&&d>=ansd)
         return 1;
     else
         return 0;
 }
 
-void dfs(int a1,int b1,int c1,int d1){
-    ansa=a1+(int)cbrt(b1)+(int)cbrt(c1)+(int)sqrt(d1);
-    ansb=b1+(int)cbrt(a1)+(int)cbrt(c1)+(int)sqrt(d1);
-    ansc=c1+(int)cbrt(a1)+(int)cbrt(b1)+(int)sqrt(d1);
-    ansd=d1+(int)sqrt(a1)+(int)sqrt(b1)+(int)sqrt(c1);
+void find(int a1,int b1,int c1,int d1){
+    ansa=a1+b1/3+c1/3+d1/2;
+    ansb=b1+a1/3+c1/3+d1/2;
+    ansc=c1+a1/3+b1/3+d1/2;
+    ansd=d1+a1/2+b1/2+c1/2;
     if(check()){
         cout<<a1<<" "<<b1<<" "<<c1<<" "<<d1;
         return;
     }
-    else{
-        for(int i=a1;i<a;i++){
-            dfs(i,b1,c1,d1);
-            for(int j=b1;j<b;j++){
-                dfs(i,j,c1,d1);
-                for(int k=c1;k<c;k++){
-                    dfs(i,j,k,d1);
-                    for(int l=d1;l<d;l++){
-                        dfs(i,j,k,l);
-                    }
-                }
-            }
-        }
-    }
+    
 }
 
 int main(){
     cin>>a>>b>>c>>d;
-    dfs(0,0,0,0);
-
+    find(0,0,0,0);
     return 0;
 }
